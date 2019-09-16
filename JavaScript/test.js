94f1e86e79a9e1e0bf14800c09e41c24cc69330c
Aidan O'Brien <obrienam@appstate.edu>
2019-09-16 10:53:00 -0400

    Played around some more
    Figured out some css styling tricks and added links via javascript.

--- JavaScript/test.js
+++ JavaScript/test.js
@@ -1,3 +1,23 @@
-let d = new Date();
-document.body.innerHTML = "<h1>" + d + "</h1>";
-document.getElementById("foot").innerHTML = "<p>" + "Footer!" + "</p>";
+document.body.onload = addElement;
+function addElement () {
+    let b = document.getElementById("body")
+    let x = document.createElement("footer");
+    x.setAttribute("id", "foot");
+    let y = document.createElement("h1");
+    let t = document.createTextNode("Website For Testing Javascript");
+    y.appendChild(t)
+    document.body.insertBefore(x,b);
+    document.body.insertBefore(y,b);
+    let footext1="Link To Github\t";
+    let footext2="Facebook\t";
+    let footext3="Twitter\t";
+    let flink1 = footext1.link("https://github.com/obrienam/Web-Practice")
+    let flink2 = footext2.link("https://www.facebook.com/aidan.obrien.77?ref=bookmarks")
+    let flink3 = footext3.link("https://twitter.com/Aidan_OBrien325")
+    document.getElementById("column1").innerHTML = `${flink1}`;
+    document.getElementById("column2").innerHTML = `${flink2}`;
+    document.getElementById("column3").innerHTML = `${flink3}`;
+    document.getElementsByTagName("h1").innerHTML = "<div>" + "Body!" + "</div>";
+    let cont = document.innerHTML;
+    console.log(cont)
+}
