fb28ec786063f0206bad413b48146da15936750e
Aidan O'Brien <obrienam@appstate.edu>
2019-10-13 18:28:13 -0400

    Added tabbed layout component.

--- Vue/Component Experiments/component-exp/src/App.vue
+++ Vue/Component Experiments/component-exp/src/App.vue
@@ -1,24 +1,52 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="My first vue app"/>
-    <VueSticker :d="224" className="example" prefix="--"></VueSticker>
+    <div id = "banner">
+      <img alt="Vue logo" src="./assets/logo.png">
+      Vue Test Website
+    </div>
+    <tabs className=".vue-tablist">
+      <tab title="Home">
+        <HelloWorld msg="A simple vuejs experimentation page."/>
+      </tab> 
+      <tab title="Stickers">
+        <div id = sticker>
+           <VueSticker :d="224" className="example" prefix="--"></VueSticker>
+        </div>
+      </tab>
+       
+    </tabs>
+    
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
 import VueSticker from 'vue-sticker'
+import { Tabs, Tab } from 'vue-slim-tabs'
 export default {
   name: 'app',
   components: {
     HelloWorld,
-    VueSticker
+    VueSticker,
+    Tabs,
+    Tab
   }
 }
 </script>
 
 <style>
+#banner {
+  transform:scale(0.5);
+  text-align: left;
+  transform-origin:left;
+  margin-top:-100px;
+  margin-bottom:-50px;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  font-size:150px;
+  width:150%
+}
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -27,6 +55,41 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+#sticker{
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 35%;
+}
+
+.vue-tablist {
+  list-style: none;
+  display: flex;
+  padding-left: 0;
+  border-bottom: 1px solid #e2e2e2;
+}
+
+.vue-tab {
+  padding: 5px 10px;
+  cursor: pointer;
+  user-select: none;
+  border: 1px solid transparent;
+  border-bottom-color: #e2e2e2;
+  border-radius: 3px 3px 0 0;
+  background-color: white;
+  position: relative;
+  bottom: -1px;
+}
+
+.vue-tab[aria-selected="true"] {
+  border-color: #e2e2e2;
+  border-bottom-color: transparent;
+}
+
+.vue-tab[aria-disabled="true"] {
+  cursor: not-allowed;
+  color: #999;
+}
 .example--main-image {
   background-image: url('assets/auto.png');
 }
@@ -34,7 +97,5 @@ export default {
   background-image: url('assets/decept.png')
 
 }
-.example--focus {
-  box-shadow: 0 0 5px 10px rgba(0,0,0,.4)
-}
+
 </style>
--- Vue/Component Experiments/component-exp/src/components/HelloWorld.vue
+++ Vue/Component Experiments/component-exp/src/components/HelloWorld.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    
-    <h3>Github source:</h3>
+    <h3>Resources:</h3>
     <ul>
       
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li><a href="https://github.com/obrienam/Web-Practice/tree/master/Vue/Component%20Experiments/component-exp" >Source Code</a></li>
+      <li><a href="https://github.com/KamilOcean/vue-sticker?ref=madewithvuejs.com" >VueSticker Component</a></li>
+      <li><a href="https://github.com/egoist/vue-slim-tabs" >Slim-Tabs Component</a></li>
+      <li><a href="https://vuejs.org/" >Vuejs Website</a></li>
     </ul>
   </div>
 </template>
